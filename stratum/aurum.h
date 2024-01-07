533d1e84119f78f2a300e79ba617c86ba471bc17
Cminor-pools <91484011+Cminor-pools@users.noreply.github.com>
2024-01-07 20:31:19 +1100

    Update aurum.h

--- stratum/aurum.h
+++ stratum/aurum.h
@@ -8,8 +8,6 @@ extern "C" {
 
 #include <stdint.h>
 
-int PHS(void *out, size_t outlen, const void *in, size_t inlen, const void *salt, size_t saltlen, unsigned int t_cost, unsigned int m_cost);
-///int (void *K, uint64_t kLen, const void *pwd, uint64_t pwdlen, const void *salt, uint64_t saltlen, uint64_t timeCost, uint64_t nRows, uint64_t nCols);
 void aurum_hash(const char* input, char* output, uint32_t size);
 
 #ifdef __cplusplus
